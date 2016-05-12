a644d93224115f347e8a2e8b03cb0c5c3ae1cb2a
mmaghajani <mma137421@gmail.com>
2016-05-12 16:10:06 +0430

    Adds logic unit

--- src/logic_unit.vhd
+++ src/logic_unit.vhd
@@ -1,1 +1,66 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all ;
+
+entity logic_unit is port(
+  a : in std_logic_vector( 7 downto 0 ) ;
+  b : in std_logic_vector( 7 downto 0 ) ;
+  and_controller : in std_logic ;
+  or_controller : in std_logic ;
+  shift_to_left_controller : in std_logic ;
+  shift_to_right_controller : in std_logic ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic;
+  carry_flag : out std_logic ;
+  sign_flag : out std_logic
+  );
+end entity ;
+
+architecture gate_level of logic_unit is 
+signal temp_in : std_logic_vector( 7 downto 0 ) ;
+signal temp_out : std_logic_vector( 7 downto 0 ) ;
+
+component and_maker is port(
+  a : in std_logic_vector( 7 downto 0 ) ;
+  b : in std_logic_vector( 7 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic;
+  carry_flag : out std_logic ;
+  sign_flag : out std_logic
+  );
+end component ;
+
+component or_maker is port(
+  a : in std_logic_vector( 7 downto 0 ) ;
+  b : in std_logic_vector( 7 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic;
+  carry_flag : out std_logic ;
+  sign_flag : out std_logic
+  );
+end component ;
+
+component shift_to_left is port(
+  a : in std_logic_vector( 7 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic;
+  carry_flag : out std_logic ;
+  sign_flag : out std_logic
+  );
+end component ;
+
+component shift_to_right is port(
+  a : in std_logic_vector( 7 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic;
+  carry_flag : out std_logic ;
+  sign_flag : out std_logic
+  );
+end component ;
+begin
+  
+end architecture ;
+
+
 
