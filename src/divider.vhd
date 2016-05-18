5f7dc0eab0f340b362b043ae8e706294cac4dcf2
mmaghajani <mma137421@gmail.com>
2016-05-18 21:15:37 +0430

    Starts implementation of divider

--- src/divider.vhd
+++ src/divider.vhd
@@ -1,1 +1,15 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all ;
 
+entity divider is port (
+  clk : in std_logic ;
+  a : in std_logic_vector( 7 downto 0 ) ;
+  b : in std_logic_vector( 7 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  avf_flag : out std_logic ;
+  zero_flag : out std_logic ;
+  sign_flag : out std_logic
+);
+end entity ;
