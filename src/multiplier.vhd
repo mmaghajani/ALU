595b1fe47cafaf1e906265019d233cf82df7edb7
mmaghajani <mma137421@gmail.com>
2016-05-18 19:20:19 +0430

    Completes multiplier

--- src/multiplier.vhd
+++ src/multiplier.vhd
@@ -16,17 +16,25 @@ end entity ;
   
 architecture gate_level of multiplier is 
 begin
+  sign_flag <= '0' ;
+  avf_flag <= '0' ;
   process( clk )
     variable b_temp : std_logic_vector( 7 downto 0 ) := b ;
     variable f : std_logic_vector( 7 downto 0 ) := "00000000" ;
-    variable sc : integer := 7 ;
+    variable sc : integer := 8 ;
     variable e : std_logic := '0' ;
     variable temp : std_logic_vector( 8 downto 0 ) ;
     begin
+      output <= f ;
+      zero_flag <= not( f(0) or f(1) or f(2) or f(3) or f(4) or f(5) or f(6) or f(7) ) ;
+      if( sc = 8 )then 
+        b_temp := b ;
+      end if ;
       if( clk'event and clk = '1' )then
+      if( sc >= 0 )then
         if( b_temp(0) = '1' )then
-          f := f + a ;
-          temp := '0'&f + '0'&a ;
+          temp := ("0"&f) + ("0"&a) ;
+          f := temp( 7 downto 0 ) ;
           e := temp(8) ;          
         end if ;
         
@@ -34,6 +42,7 @@ begin
         f := e & f( 7 downto 1 ) ;
         e := '0' ;
         sc := sc - 1 ;
+      end if ;
       end if ;
     end process ;
 end architecture ;
