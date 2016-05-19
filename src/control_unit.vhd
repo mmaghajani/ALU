12c3c4f04e8a3d14d759372737178d4c77169eb0
mmaghajani <mma137421@gmail.com>
2016-05-19 14:17:47 +0430

    Adds controll unit

--- src/control_unit.vhd
+++ src/control_unit.vhd
@@ -1,1 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all ;
+
+entity control_unit is port (
+  clk : in std_logic ;
+  a : in std_logic_vector( 7 downto 0 ) ;
+  b : in std_logic_vector( 7 downto 0 ) ;
+  selector : in std_logic_vector( 2 downto 0 ) ;
+  output : out std_logic_vector( 7 downto 0 ) ;
+  zero_flag : out std_logic ;
+  sign_flag : out std_logic ;
+  avf_flag : out std_logic 
+  );
+end entity ;
 
